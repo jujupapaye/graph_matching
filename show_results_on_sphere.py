53dc0ee115e838fe5fe4fc9ea917807eba138bf5
jujupapaye <juliette.philibert@gmail.com>
2019-08-30 15:43:13 +0200

    show

--- show_results_on_sphere.py
+++ show_results_on_sphere.py
@@ -61,40 +61,41 @@ def show_sphere(matching_list, graphs):
     """
     sphere_mesh = trim.primitives.Sphere(radius=100)
 
-    nb_patients = matching_list.shape[0]
+    nb_graphs = matching_list.shape[0]
     nb_pits = matching_list[0].shape[0]
 
     pit_col_val = np.arange(0, nb_pits, 1)
     textures = np.ones(sphere_mesh.vertices.shape[0])*nb_pits
 
     # tri des résultats pour faciliter l'interpretation après
-    for p in range(nb_patients):
+    for p in range(nb_graphs):
         matching_list[p] = matching_list[p][np.argsort(matching_list[p][:, 1])]
 
     trans_pit0 = np.eye(4)
     trans_pit1 = np.eye(4)
 
     for pit in range(0, nb_pits):
-        x0 = graphs[0].nodes[pit]['coord'][0]
-        y0 = graphs[0].nodes[pit]['coord'][1]
-        z0 = graphs[0].nodes[pit]['coord'][2]
-        pit0 = trim.primitives.Sphere(radius=1, subdivisions=0)
-        trans_pit0[:, 3] = [x0, y0, z0, 1]
-        pit0.apply_transform(trans_pit0)
-        textures = np.concatenate((textures, np.ones(pit0.vertices.shape[0]) * pit_col_val[pit]))
-        sphere_mesh += pit0
+        if graphs[0].has_node(pit):
+            x0 = graphs[0].nodes[pit]['coord'][0]
+            y0 = graphs[0].nodes[pit]['coord'][1]
+            z0 = graphs[0].nodes[pit]['coord'][2]
+            pit0 = trim.primitives.Sphere(radius=1, subdivisions=0)
+            trans_pit0[:, 3] = [x0, y0, z0, 1]
+            pit0.apply_transform(trans_pit0)
+            textures = np.concatenate((textures, np.ones(pit0.vertices.shape[0]) * pit_col_val[pit]))
+            sphere_mesh += pit0
 
-        for pat in range(nb_patients):
+        for pat in range(nb_graphs):
+            if graphs[pat].has_node(matching_list[pat][pit, 0]):
+                x1 = graphs[pat].nodes[matching_list[pat][pit, 0]]['coord'][0]
+                y1 = graphs[pat].nodes[matching_list[pat][pit, 0]]['coord'][1]
+                z1 = graphs[pat].nodes[matching_list[pat][pit, 0]]['coord'][2]
+                pit1 = trim.primitives.Sphere(radius=1, subdivisions=0)
+                trans_pit1[:, 3] = [x1, y1, z1, 1]
+                pit1.apply_transform(trans_pit1)
+                textures = np.concatenate((textures, np.ones(pit1.vertices.shape[0]) * pit_col_val[pit]))
 
-            x1 = graphs[pat].nodes[matching_list[pat][pit, 0]]['coord'][0]
-            y1 = graphs[pat].nodes[matching_list[pat][pit, 0]]['coord'][1]
-            z1 = graphs[pat].nodes[matching_list[pat][pit, 0]]['coord'][2]
-            pit1 = trim.primitives.Sphere(radius=1, subdivisions=0)
-            trans_pit1[:, 3] = [x1, y1, z1, 1]
-            pit1.apply_transform(trans_pit1)
-            textures = np.concatenate((textures, np.ones(pit1.vertices.shape[0]) * pit_col_val[pit]))
-
-            sphere_mesh += pit1
+                sphere_mesh += pit1
 
     splt.pyglet_plot(sphere_mesh, values=textures, color_map='gist_ncar', plot_colormap=False, caption='test')
 
@@ -106,7 +107,7 @@ def pits_sphere(graphs):
 
     :param graphs: list of pits graphs
     """
-    color_hex = np.array(['#000000', '#6e2c00', '#7d6608', '#186a3b', '#1b4f72', '#4a235a', '#641e16', '#ecf0f1',
+    color_hex = np.array(['#000000', '#1b4f72', '#4a235a', '#641e16', '#ecf0f1',
                           '#ecf0f1', '#f1c40f', '#27ae60', '#48c9b0', '#2980b9', '#cb4335', '#af7ac5', '#a3e4d7', '#fad7a0', '#f2d7d5'])
     sphere_mesh = trim.primitives.Sphere(radius=100)
 
