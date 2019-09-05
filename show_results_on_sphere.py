1d29bfcaf90a5a4fcf9a504c8fba04138c2000ba
jujupapaye <juliette.philibert@gmail.com>
2019-09-05 13:37:58 +0200

    final show

--- show_results_on_sphere.py
+++ show_results_on_sphere.py
@@ -2,33 +2,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 import trimesh as trim
 import slam.plot as splt
-import util
-
-
-def metric_geodesic(match, graphs):
-    """
-    Calcule la moyenne des distances géodésiques entre chaque points matchés entre eux
-    :param match: list des matching
-    :param graphs: list de graphes matchés
-    :return: d
-    """
-    # tri des résultats pour faciliter l'interpretation après
-    nb_graphs = len(graphs)
-    for p in range(nb_graphs):
-        match[p] = match[p][np.argsort(match[p][:, 1])]
-
-    d = 0
-    nb_pair = 0
-    for m in range(1, len(match)):
-        for i in range(len(match[m])):
-            p1 = i
-            p2 = match[m, i, 0]
-            if graphs[0].has_node(p1) and graphs[m].has_node(p2):
-                nb_pair += 1
-                xA, yA, zA = graphs[0].node[p1]['coord']
-                xB, yB, zB = graphs[m].node[p2]['coord']
-                d += util.dist_on_sphere(100, xA, yA, zA, xB, yB, zB)
-    return d / nb_pair
 
 
 def show_sphere_for_2(matching, g0, g1):
